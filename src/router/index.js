2812dc0808748cbd23a00fa01ace55e43b9bd89e
dengshaomin86 <1101869752@qq.com>
2020-04-19 15:29:30 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+const context = require.context("../views", false, /.vue$/);
+const defaultRouter = context.keys().map(key => {
+  const name = key.substring(key.indexOf("/") + 1, key.lastIndexOf(".vue"));
+  return {
+    path: key.substring(key.indexOf("/"), key.lastIndexOf(".vue")),
+    name,
+    component: context(key).default,
+    meta: {
+      title: name
+    }
+  };
+});
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      component: () => import("../views/home.vue")
+    },
+    ...defaultRouter
+  ]
+});
